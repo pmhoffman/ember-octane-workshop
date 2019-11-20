fe61f94f3b5d2445c3e26e92387ce296e228380c
Pamela Hoffman <poolgirlblue@gmail.com>
2019-11-19 19:33:25 -0600

    Completed note 4 - helpers

---
+++ tests/integration/helpers/format-timestamp-test.js
@@ -0,0 +1,25 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Helper | format-timestamp', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('01-01-2019', async function(assert) {
+    this.set('inputValue', '01-01-2019');
+
+    await render(hbs`{{format-timestamp inputValue}}`);
+
+    assert.equal(
+      this.element.textContent.trim(),
+      'Jan 1, 2019 00:00.00 AM'
+    );
+  });
+
+  test('No argument passed', async function(assert) {
+    await render(hbs`{{format-timestamp}}`);
+    assert.equal(this.element.textContent.trim(), '(unknown)');
+  });
+});
